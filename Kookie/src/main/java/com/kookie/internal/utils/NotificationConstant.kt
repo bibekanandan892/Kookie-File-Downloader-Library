b95f56c043d05684e028df1cf463b83e6f65e153
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2024-11-06 01:44:31 +0530

    Added notification constant

---
+++ Kookie/src/main/java/com/kookie/internal/utils/NotificationConstant.kt
@@ -0,0 +1,30 @@
+package com.kookie.internal.utils
+
+const val DOWNLOAD_NOTIFICATION_CHANNEL_ID = "download_channel_v2"
+const val DEFAULT_NOTIFICATION_ENABLED = false
+const val NOTIFICATION_CHANNEL_NAME_KEY = "notification_channel_name_key"
+const val DEFAULT_NOTIFICATION_CHANNEL_NAME = "Download Progress"
+const val NOTIFICATION_CHANNEL_DESCRIPTION_KEY = "notification_channel_description_key"
+const val DEFAULT_NOTIFICATION_CHANNEL_DESCRIPTION = "Notification for file download status"
+const val NOTIFICATION_CHANNEL_IMPORTANCE_KEY = "notification_channel_importance_key"
+const val DEFAULT_NOTIFICATION_CHANNEL_IMPORTANCE = 2 // LOW
+const val SMALL_NOTIFICATION_ICON_KEY = "small_notification_icon_key"
+const val DEFAULT_SMALL_NOTIFICATION_ICON = -1
+const val NOTIFICATION_ID_KEY = "notification_id_key"
+
+// Actions
+const val NOTIFICATION_DISMISSED_ACTION = "notification_dismissed_action"
+const val DOWNLOAD_COMPLETED_ACTION = "download_completed_action"
+const val DOWNLOAD_FAILED_ACTION = "download_failed_action"
+const val DOWNLOAD_CANCELLED_ACTION = "download_cancelled_action"
+const val DOWNLOAD_PAUSED_ACTION = "download_paused_action"
+const val NOTIFICATION_RESUME_ACTION = "notification_resume_action"
+const val NOTIFICATION_RETRY_ACTION = "notification_retry_action"
+const val NOTIFICATION_PAUSE_ACTION = "notification_pause_action"
+const val NOTIFICATION_CANCEL_ACTION = "notification_cancel_action"
+
+// Cancel
+const val CANCEL_BUTTON_LABEL = "Cancel"
+const val PAUSE_BUTTON_LABEL = "Pause"
+const val RESUME_BUTTON_LABEL = "Resume"
+const val RETRY_BUTTON_LABEL = "Retry"
