39269d96f00f314bad31db6f3e39f87ad52bf892
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2024-11-06 01:43:15 +0530

    Added download constant

--- Kookie/src/main/java/com/kookie/internal/utils/DownloadConstant.kt
+++ Kookie/src/main/java/com/kookie/internal/utils/DownloadConstant.kt
@@ -2,3 +2,21 @@ package com.kookie.internal.utils
 
 const val RANGE_HEADER= "Range"
 const val HTTP_STATUS_RANGE_NOT_SATISFIABLE = 416
+const val READ_TIMEOUT_MS = 10000L
+const val CONNECT_TIMEOUT_MS = 10000L
+const val BASE_API_URL = "http://localhost/"
+const val DOWNLOAD_TAG = "downloads"
+const val FILE_NAME_KEY = "key_fileName"
+const val STATE_KEY = "key_state"
+const val PROGRESS_KEY = "key_progress"
+const val MAX_PROGRESS_VALUE = 100
+const val PROGRESS_STATUS = "progress"
+const val STARTED_STATUS = "started"
+const val LENGTH_KEY = "key_length"
+const val DEFAULT_LENGTH_VALUE = 0L
+const val REQUEST_ID_KEY = "key_request_id"
+const val DOWNLOAD_REQUEST_KEY = "key_download_request"
+const val NOTIFICATION_CONFIG_KEY = "key_notification_config"
+const val ETAG_HEADER_KEY = "ETag"
+const val CONTENT_LENGTH_HEADER = "Content-Length"
+
